7f5cbbbcd7c93382e87dcbad619e6fadc74aa536
Denis Bajet <dbajet@gmail.com>
2019-12-07 16:13:07 +0000

    implementation with one class per log file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='sight_machine',
-    version='0.2',
+    version='0.3',
     author="Denis Bajet",
     author_email="dbajet@gmail.com",
     description='Simple binary logger',
